7720c4441acac42e06476c7d685e1e91301559f0
jen <jenny.diep@icloud.com>
2021-12-11 16:48:54 -0500

    adding buttons
    review button alignements

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
+import Button from './Button';
 
 function App() {
   return (
     <div className="container">
       <h1>Pomodoro</h1>
       <small>Let's get stuff done</small>
+    
+      <br></br>
+
+      <div className='buttons'>
+        <Button title={"Start"}/>
+        <Button title={"Pause"}/>
+      </div>
+  
     </div>
   );
 }
---
+++ src/Button.js
@@ -0,0 +1,11 @@
+const Button = ({title, activeClass, _callback}) => {
+    return (
+        <div>
+            <button className= {activeClass} onClick={_callback}>
+                {title}
+            </button>
+        </div>
+    )
+}
+
+export default Button;
